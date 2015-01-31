40333339bcd0908ec0470a2c77d51f19918fc90a
kim00kim <kimdehitta@gmail.com>
2015-01-31 21:04:41 +0800

    - removed ionic rating

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'jobPortl.services' is found in services.js
 // 'jobPortl.controllers' is found in controllers.js
-angular.module('jobPortl', ['ionic', 'ionic.rating', 'jobPortl.controllers', 'jobPortl.services', 'jobPortl.directives'])
+angular.module('jobPortl', ['ionic', 'jobPortl.controllers', 'jobPortl.services', 'jobPortl.directives'])
 
 	//})
 	/*.config(function($compileProvider){
