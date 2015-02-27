c80e3adef608ed95b8394ee0118272b82f31d9fe
kim <kimdehitta@gmail.com>
2015-02-27 20:40:35 +0800

    - change url

--- www/js/services.js
+++ www/js/services.js
@@ -1,6 +1,6 @@
 angular.module('jobPortl.services', [])
 
-	.constant('baseUrl', 'http://10.20.1.198/jobportl/web/api/')
+	.constant('baseUrl', 'http://192.168.43.244/jobportl/web/api/')
 
 	.factory('$localStorage', ['$window', function ($window) {
 		return {
